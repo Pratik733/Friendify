964363e1a90eeba924bd14840fcaea424fabf685
Pratik733 <pratik.tayade733@gmail.com>
2023-01-09 18:16:03 +0530

    cors 2

--- server/index.js
+++ server/index.js
@@ -23,7 +23,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
